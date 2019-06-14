3d8c2626164ae608df963fd491b71e9af7c40310
Kaloyan Madjunov <k.madjunov@gmail.com>
2019-06-14 22:34:35 +0300

    Create endpoint for getting a single product with id from db

--- src/main/java/yamly/controllers/ProductController.java
+++ src/main/java/yamly/controllers/ProductController.java
@@ -1,14 +1,38 @@
 package yamly.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import yamly.models.Product;
+import yamly.services.ProductService;
 
 @RestController
 @EnableAutoConfiguration
 public class ProductController {
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
+    public static final Logger LOGGER = LoggerFactory.getLogger(ProductController.class);
+
+    @Autowired
+    private ProductService productService;
+
+    @RequestMapping(value = "/products/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> getProduct(@PathVariable("id") Integer id) {
+        LOGGER.info("Fetching Product with id {}", id);
+
+        if(!this.productService.productExists(id)) {
+            LOGGER.error("Product with id {} not found.", id);
+            return new ResponseEntity(new CustomErrorType("Product with id " + id
+                    + " not found"), HttpStatus.NOT_FOUND);
+        }
+
+        Product product = this.productService.findProduct(id);
+
+        return new ResponseEntity<Product>(product, HttpStatus.OK);
     }
 }

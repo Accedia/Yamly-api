a16ec42f2069ee021ed077e62143ae560b071a77
Kaloyan Madjunov <k.madjunov@gmail.com>
2019-06-14 22:33:54 +0300

    Create custom error type class for printing error messages

---
+++ src/main/java/yamly/controllers/CustomErrorType.java
@@ -0,0 +1,13 @@
+package yamly.controllers;
+
+public class CustomErrorType {
+    private String errorMessage;
+
+    public CustomErrorType(String errorMessage){
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+}

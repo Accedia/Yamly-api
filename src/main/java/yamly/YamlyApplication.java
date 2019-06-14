99d17d31566648e48c99b36b687158a6f3247ce0
Kaloyan Madjunov <k.madjunov@gmail.com>
2019-06-14 21:19:49 +0300

    Impement run configuration and a home endpoint

---
+++ src/main/java/yamly/YamlyApplication.java
@@ -0,0 +1,11 @@
+package yamly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamlyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(YamlyApplication.class, args);
+    }
+}
---
+++ src/main/java/yamly/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package yamly.controllers;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+public class HomeController {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+}

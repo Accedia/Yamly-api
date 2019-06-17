32f4ac20f257d5a3245cb339c7ca066fed6108ba
Kaloyan Madjunov <k.madjunov@gmail.com>
2019-06-17 03:25:01 +0300

    Create Recipe Repository with custom method for getting all liked from the firebase user recipes

---
+++ src/main/java/yamly/repositories/RecipeRepository.java
@@ -0,0 +1,13 @@
+package yamly.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import yamly.models.Recipe;
+
+import java.util.List;
+
+public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
+    @Query(value = "SELECT recipes_id FROM yamly.products_recipes WHERE products_id IN :liked AND products_id NOT IN :disliked", nativeQuery = true)
+    List<Integer> findAllIdsOfRecipesWithLikedProducts(@Param("liked") List<Integer> liked, @Param("disliked") List<Integer> disliked);
+}
